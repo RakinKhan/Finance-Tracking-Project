65376f286bfc187ececfcb1ee19f6930a70abe32
RakinKhan <rakinalikhan@gmail.com>
2021-09-07 22:41:25 -0400

    table column width adjusted. Chart.tsx updated so that it can take all transactions, separate them into buys and sells, and display them as bars on a time series graph.

--- src/Components/UI/Dashboard/MiddleBar/Chart/Chart.tsx
+++ src/Components/UI/Dashboard/MiddleBar/Chart/Chart.tsx
@@ -1,21 +1,49 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import "chartjs-adapter-moment";
+
 const Chart = (props: any) => {
+  const transactions = props.transaction;
+  const buy = [] as any;
+  const sell = [] as any;
+  transactions.forEach((transaction: any) => {
+    let x = new Date(transaction.date);
+    let y = transaction.amount;
+    if (transaction.type === "BUY") {
+      return buy.push({
+        x: x,
+        y: y,
+      });
+    } else {
+      return sell.push({
+        x: x,
+        y: y,
+      });
+    }
+  });
+  console.log(buy);
+  console.log(sell);
   const buys = [
-    { x: "2021-06-24", y: 30 },
+    { x: new Date("Wed Jul 14 2021"), y: 30 },
     { x: "2021-06-25", y: 20 },
     { x: "2021-06-26", y: 15 },
     { x: "2021-06-27", y: 20 },
     { x: "2021-07-28", y: 10 },
   ] as any;
   const data = {
-    type: "bar",
     datasets: [
       {
-        label: "Hello",
-        data: buys,
-        borderColor: "green",
+        type: "bar",
+        label: "Buy",
+        data: buy,
+        backgroundColor: "green",
+        fill: false,
+      },
+      {
+        type: "bar",
+        label: "Sell",
+        data: sell,
+        backgroundColor: "purple",
         fill: false,
       },
     ],
@@ -44,7 +72,7 @@ const Chart = (props: any) => {
                 displayFormats: {
                   quarter: "MMM YYYY",
                 },
-                unit: "day",
+                unit: "month",
               },
             },
             y: {
--- src/Components/UI/Dashboard/MiddleBar/TransactionTable/TransactionTable.tsx
+++ src/Components/UI/Dashboard/MiddleBar/TransactionTable/TransactionTable.tsx
@@ -6,15 +6,15 @@ const TransactionTable = (props: any) => {
       <table>
         <thead>
           <tr>
-            <th style={{ width: "10%" }} scope="col">
+            <th style={{ width: "15%" }} scope="col">
               Date
             </th>
             <th scope="col">Account</th>
             <th scope="col">Stock</th>
             <th scope="col">Shares</th>
             <th scope="col">Amount</th>
-            <th scope="col" style={{ width: "20%" }}>
-              Currency Conv. Fee
+            <th scope="col" style={{ width: "15%" }}>
+              Currency Fee
             </th>
             <th scope="col">Trans. Fee</th>
             <th scope="col">Type</th>
@@ -23,12 +23,14 @@ const TransactionTable = (props: any) => {
         <tbody>
           {props.transactions.map((transaction: any) => (
             <tr key={transaction.key}>
-              <td>{`${transaction.date.getDate()}-${transaction.date.getMonth()}-${transaction.date.getFullYear()}`}</td>
+              <td
+                style={{ width: "15%" }}
+              >{`${transaction.date.getDate()}-${transaction.date.getMonth()}-${transaction.date.getFullYear()}`}</td>
               <td>{transaction.account}</td>
               <td>{transaction.stock}</td>
               <td>{transaction.shares}</td>
               <td>{transaction.amount}</td>
-              <td style={{ width: "20%" }}>{transaction.convFee}</td>
+              <td style={{ width: "15%" }}>{transaction.convFee}</td>
               <td>{transaction.transFee}</td>
               <td>{transaction.type}</td>
             </tr>
