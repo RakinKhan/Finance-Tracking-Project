fe66900cb40fc28c6fe06cab71c7f7e91af5c5da
RakinKhan <rakinalikhan@gmail.com>
2021-09-18 23:27:05 -0400

    made a slight modification to the TopFive.tsx component.

--- src/Components/UI/Dashboard/RightSideBar/TopFive/TopFive.tsx
+++ src/Components/UI/Dashboard/RightSideBar/TopFive/TopFive.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./TopFive.css";
 const TopFive = (props: any) => {
+  const transactions = props.transactions;
   return (
     <div className={"divstyle"}>
       <div className={"top-performers-header"}>Top Performers</div>
       <div className={"topfive"}>
-        <div className={"topfive-card"}>Hi</div>
-        <div className={"topfive-card"}>there</div>
-        <div className={"topfive-card"}>how</div>
-        <div className={"topfive-card"}>Are</div>
-        <div className={"topfive-card"}>You</div>
+        <div className={"topfive-card"}>{}</div>
+        <div className={"topfive-card"}>{}</div>
+        <div className={"topfive-card"}>{}</div>
+        <div className={"topfive-card"}>{}</div>
+        <div className={"topfive-card"}>{}</div>
       </div>
     </div>
   );
