7cb9c16ac0b0ff4c418da960383a10825bc575c8
RakinKhan <rakinalikhan@gmail.com>
2022-03-20 19:12:20 -0400

    Finally solved the rendering issue for this component so that it will correctly render the price and changes conditionally once the Api data is recieved completely and processes. still have to figure out how to rerender on specific intervals.

--- src/Components/UI/Dashboard/RightSideBar/TopFive/TopFive.tsx
+++ src/Components/UI/Dashboard/RightSideBar/TopFive/TopFive.tsx
@@ -1,21 +1,21 @@
-import React, {useContext, useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import ChartComponent from "react-chartjs-2";
 import "./TopFive.css";
 
 const TopFive = (props: any) => {
   const changeComponent = useRef(0);
+  const comp = useRef(false)
   const averages = props.averages;
   const labels = props.labels;
-  const [currentPrices,setCurrentPrices] = useState([] as any);
-
+  const currentPrices = useRef([]as any)
+  const [pall, setPall] = useState([]as any)
   let calculatedAverages = [] as any;
-  console.log(averages)
-  console.log(currentPrices)
+
   calculatedAverages.sort((a: any, b: any) => {
     return b.change - a.change;
   });
   averages.forEach((average: any) => {
-    currentPrices.forEach((currentPrice: any) => {
+    pall.forEach((currentPrice: any) => {
       if (currentPrice.stock === average.stock) {
         calculatedAverages.push({
           stock: average.stock,
@@ -29,26 +29,31 @@ const TopFive = (props: any) => {
     });
   });
   useEffect(() => {
-    changeComponent.current = 1
+
     const currentPriceAll = async (labels:any) => {
       const pAll = await Promise.all(labels.map((label: any) => {
-        const response =  fetch(`https://finnhub.io/api/v1/quote?symbol=${label}&token=bprteb7rh5r8s3uvb2ag`).then((res:any) => res.json()).then((res:any) => {return {
+        const response = fetch(`https://finnhub.io/api/v1/quote?symbol=${label}&token=bprteb7rh5r8s3uvb2ag`).then((res:any) => res.json()).then((res:any) => {return {
           stock: label,
           price: res.c
         }})
         return response
       }))
-      setCurrentPrices([...pAll])
+      setPall(pAll)
     }
-    setTimeout(() => currentPriceAll(labels), 10000)
-  })
-  useEffect((() => {
-    if (labels.length !=0) {
-      changeComponent.current = 2
+    if (labels.length > 0) {
+      currentPriceAll(labels)
+      comp.current=true
+      changeComponent.current = 1
     }
-  }), [currentPrices])
+  }, [labels])
+  
 
 
+  if (comp.current === true && changeComponent.current === 1) {
+    comp.current = false;
+    changeComponent.current = 2
+    console.log(pall)
+  }
   return (
     <div className={"divstyle"}>
       <div className={"top-performers-header"}>Top Performers</div>
